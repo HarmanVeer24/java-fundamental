4213beae12c05e71c9460cf7a825d82037f937a9
HarmanVeer24 <harmanveer0024@gmail.com>
2025-02-25 14:13:42 +0530

    printing message changed

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,5 @@
 public class FirstJavaProgram{
 	public static void main(String args[]){
-		System.out.println("Capegemini training started");
+		System.out.println("BridgeLabz training started");
 	}
 }
