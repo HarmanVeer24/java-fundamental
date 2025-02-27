d2528ef0e3326801aa5ebf5a1ae0d5907fac476f
HarmanVeer24 <harmanveer0024@gmail.com>
2025-02-27 13:43:35 +0530

    Printing the addition of two numbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 class AddTwoNumbers{
         public static void main(String[] args){
-                Scanner sc = new Scanner(System.in);
-                int FirstNumber = sc.nextInt();
-                int SecondNumber = sc.nextInt();
-                int Addition = FirstNumber + SecondNumber;
-                System.out.println(Addition);
-                }
+        Scanner sc = new Scanner(System.in);
+        int FirstNumber = sc.nextInt();
+        int SecondNumber = sc.nextInt();
+        int Addition = FirstNumber + SecondNumber;
+        System.out.println(Addition);
         }
+}
 
 
