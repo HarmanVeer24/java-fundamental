c5b8952d2a41bb15803c10fc412becefb35ffd90
HarmanVeer24 <harmanveer0024@gmail.com>
2025-02-27 13:21:15 +0530

    Printing the result after Power Calculation

---
+++ PowerCalculation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class PowerCalculation{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int Base = sc.nextInt();
+		int Exponent = sc.nextInt();
+		int result = (int)Math.pow(Base,Exponent);
+		System.out.println(result);
+		}
+	}
