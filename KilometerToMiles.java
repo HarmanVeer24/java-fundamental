ccbfef049198a2591791bced365897712c8d61d5
HarmanVeer24 <harmanveer0024@gmail.com>
2025-02-27 13:51:38 +0530

    Printing the Kilometer to Miles conversion

--- KilometerToMiles.java
+++ KilometerToMiles.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 class KilometerToMiles{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int Kilometer = sc.nextInt();
-		double Miles = Kilometer * 0.621371;
-		System.out.println(Miles);
-		}
+	Scanner sc = new Scanner(System.in);
+	int Kilometer = sc.nextInt();
+	double Miles = Kilometer * 0.621371;
+	System.out.println(Miles);
 	}
+}
 
