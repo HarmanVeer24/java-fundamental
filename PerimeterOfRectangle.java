360ca4bf54338636eafa5fc907e02814f05e8522
HarmanVeer24 <harmanveer0024@gmail.com>
2025-02-27 13:01:30 +0530

    Printing the Parameter

---
+++ PerimeterOfRectangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class PerimeterOfRectangle{
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                int Length = sc.nextInt();
+                int Width = sc.nextInt();
+                int Perimeter = 2*(Length+Width);
+                System.out.println(Perimeter);
+                }
+        }
+
