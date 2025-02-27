3ce74cb0b0610aa5b45c65ed56209eab6be4b53a
HarmanVeer24 <harmanveer0024@gmail.com>
2025-02-27 13:48:43 +0530

    Printing the Area of Circle

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 class AreaOfCircle{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int Radius = sc.nextInt();
-		double Area = Radius * Radius *3.14;
-		System.out.println(Area);
-		}
+	Scanner sc = new Scanner(System.in);
+	int Radius = sc.nextInt();
+	double Area = Radius * Radius *3.14;
+	System.out.println(Area);
 	}
+}
